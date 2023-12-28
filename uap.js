863c60505b6f8e367571cbf43e82fa3a5f506c16
rapepp <108265619+rapepp@users.noreply.github.com>
2023-12-28 20:12:27 +0700

    uap.js

--- uap.js
+++ uap.js
@@ -1,51 +1,51 @@
-let barang;
-[
-    {
-        kode : "001",
-        nama : "A",
-        harga : 10000
-    },
-    {
-        kode :"002",
-        nama : "B",
-        harga : 20000
-    },
-    {
-        kode : "003",
-        nama : "C",
-        harga : 30000
-    }
-]
-        
-function cariBarang(kode) {
-    for(let i = 0; i < barang.length; i++) {
-        if(barang[i].kode === kode) {
-        return barang[i];
+    let barang = [
+        {
+            kode: "001", 
+            nama: "A",
+            harga: 10000  
+        },
+        {
+            kode: "002", 
+            nama: "B", 
+            harga: 20000
+        },
+        {
+            kode: "003",
+            nama: "C", 
+            harga: 30000
         }
-    }
-    return null;
-    }
+        ];
+        
+        function cariBarang(kode) {
+        for(let i = 0; i < barang.length; i++) {
+            if(barang[i].kode === kode) {
+            return barang[i];
+            }
+        }
+        return null;
+        }
+        
+        function proses() {
+        
+        let kode = document.getElementById("kode").value;  
+        let jumlah = parseInt(document.getElementById("jumlah").value);
     
-    function proses() {
+        let dataBarang = cariBarang(kode);
+        
+        if(!dataBarang) {
+            alert("Kode barang tidak ditemukan!");
+            return;  
+        }
     
-    let kode = document.getElementById("kode").value;  
-    let jumlah = parseInt(document.getElementById("jumlah").value);
+          let total = dataBarang.harga * jumlah;
 
-    let dataBarang = cariBarang(kode);
+        alert("Total yang harus dibayar: Rp." + total); 
     
-    if(!dataBarang) {
-        alert("Kode barang tidak ditemukan!");
-        return;  
-    }
-
-      let total = dataBarang.harga * jumlah;
-
-    alert("Total yang harus dibayar: Rp." + total); 
-
-    let bayar = parseInt(prompt("Uang yang dibayar:"));
-
-    let kembalian = bayar - total;
-
-    alert("Kembalian: Rp." + kembalian);
+        let bayar = parseInt(prompt("Uang yang dibayar:"));
     
-    }
+        let kembalian = bayar - total;
+
+        alert("Kembalian: Rp." + kembalian);
+        
+        }
+    
